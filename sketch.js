41a7fd5c1fd000a7c5ad328cb68fbf41bafdd683
JaessScript <jaess.script@gmail.com>
2020-06-17 19:40:14 +0200

    Add text

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 let canvas;
 let next;
 let tv;
+let info;
+let bg_info;
+let x = 70;
+let y = 50;
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
@@ -17,16 +21,32 @@ function setup() {
 	next.style('font-size', '1.5em');
 	setInterval(changeColor, 500);
 
+	info = select('#info');
+	info.style('color', 'black');
+	info.position(x, y);
+	info.style('font-size', '1.5em');
+
+	bg_info = select('#bg_info');
+	bg_info.style('background-color', 'white');
+
 	tv = select('#tv');
-	tv.position(windowWidth / 3, windowHeight / 2);
-	tv.style('width:', '100%');
-	tv.style('height:', 'auto');
+	tv.position(windowWidth / 4, windowHeight / 2);
+	tv.style('max-width', '100%');
+	tv.style('max-height', '100%');
+
+	setInterval(changePosition, 200);
 }
 
 function changeColor() {
 	let colors = ['Red', 'Orange', 'Yellow', 'MediumSpringGreen', 'RoyalBlue', 'Purple', 'Pink', 'LightCyan'];
 	let col = random(colors);
 	next.style('background-color', col);
+}
+
+function changePosition() {
+	info.position(x, y);
+	x = constrain(x + random(-2, 2), 0, windowWidth);
+	y = constrain(y + random(-2, 2), 0, windowHeight + 5);
 }
 
 function draw() {
