9160667eb997e6f0063603b585d9015911e2a4d6
JaessScript <jaess.script@gmail.com>
2020-06-19 13:35:59 +0200

    Add another image

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let canvas;
 let next;
 let tv;
+let redcctvs;
 let info;
 let bg_info;
 let x = 70;
@@ -25,9 +26,15 @@ function setup() {
 	info.style('color', 'black');
 	info.position(x, y);
 	info.style('font-size', '1.5em');
+	info.style('font-family', 'courier');
 
 	bg_info = select('#bg_info');
 	bg_info.style('background-color', 'white');
+
+	tv = select('#redcctvs');
+	tv.position(windowWidth / 2, windowHeight / 5);
+	tv.style('max-width', '100%');
+	tv.style('max-height', '100%');
 
 	tv = select('#tv');
 	tv.position(windowWidth / 4, windowHeight / 2);
